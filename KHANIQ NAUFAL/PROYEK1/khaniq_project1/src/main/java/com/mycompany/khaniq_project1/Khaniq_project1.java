9c564b7074cbac805b6b7b074bbb299073d07234
nopaalkh <khaniqnaufal@gmail.com>
2024-09-01 08:47:30 +0700

    first commit

---
+++ KHANIQ NAUFAL/PROYEK1/khaniq_project1/src/main/java/com/mycompany/khaniq_project1/Khaniq_project1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.khaniq_project1;
+
+/**
+ *
+ * @author hp
+ */
+public class Khaniq_project1 {
+    public static void main(String[] args) {
+        // Ganti dengan data kalian
+        String nama = "KHANIQ NAUFAL";
+        String npm = "2340506071";
+        String alamat = "WUWUHARJO, KAJORAN";
+        String noHP = "085************";
+        
+        // Menampilkan informasi
+        System.out.println("• Nama   : " + nama);
+        System.out.println("• NPM    : " + npm);
+        System.out.println("• Alamat : " + alamat);
+        System.out.println("• No_HP  : " + noHP);
+    }
+}
